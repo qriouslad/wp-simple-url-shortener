1e819e863bb7a97c38747bfe798882494243d9e3
CodeGood <codegood.io@gmail.com>
2017-06-21 11:50:13 +0000

    Initial commit: plugin header.

---
+++ wp-simple-url-shortener.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: WP Simple URL Shortener
+Plugin URI:  https://github.com/qriouslad/wp-simple-url-shortener
+Description: Simple URL shortener using goo.gl
+Version:     1.0
+Author:      Bowo
+Author URI:  https://bowo.io
+Text Domain: wpsus
+Domain Path: /languages
+License:     GPL2
+ 
+WP Simple URL Shortener is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+WP Simple URL Shortener is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with {Plugin Name}. If not, see {License URI}.
+*/
