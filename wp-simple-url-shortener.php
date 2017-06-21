607f7891ec4806c42209aa785ed9128239d33e9b
CodeGood <codegood.io@gmail.com>
2017-06-21 13:48:33 +0000

    Add settings page to input API key.

--- wp-simple-url-shortener.php
+++ wp-simple-url-shortener.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Simple URL Shortener
 Plugin URI:  https://github.com/qriouslad/wp-simple-url-shortener
-Description: Simple URL shortener using goo.gl
+Description: Simple URL shortener using Google’s URL Shortener API. Automaticall add short url for each post.
 Version:     1.0
 Author:      Bowo
 Author URI:  https://bowo.io
@@ -23,3 +23,50 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with {Plugin Name}. If not, see {License URI}.
 */
+
+/* Google Short URL API key: AIzaSyAdlHJA0F86Xq8EkkdmTN5BZlRpIcpVCMw 
+ *
+ * API explorer: https://developers.google.com/apis-explorer/?hl=en_US#p/urlshortener/v1/
+ */
+
+/*
+ * Functions to create settings page to enter and save API key
+ * 
+ * @see https://developer.wordpress.org/plugins/settings/settings-api/
+ */
+
+function wpsus_settings_page() {
+	add_settings_section('section', "Enter Key Details", null, 'wpsus');
+	add_settings_field('wpsus-input-field', 'API Key', 'wpsus_input_field_display', 'wpsus', 'section');
+	register_setting('section', 'wpsus-input-field');
+}
+
+function wpsus_input_field_display() {
+ ?>
+ 	<input type="text" name="wpsus-input-field" value="<?php echo get_option('wpsus-input-field'); ?>" style="width:50%;" />
+ <?php
+}
+
+add_action('admin_init', 'wpsus_settings_page');
+
+function wpsus_page() {
+	?>
+		<div class="wrap">
+			<h1>URL Shortener Settings</h1>
+			<form method="post" action="options.php">
+				<?php
+					settings_fields('section');
+					do_settings_sections('wpsus');
+					submit_button();
+				?>
+			</form>
+		</div>
+	<?php
+}
+
+function wpsus_menu_item() {
+	add_submenu_page('options-general.php', 'URL Shortener', 'URL Shortener', "manage_options", 'url-shortener', 'wpsus_page');
+}
+
+add_action('admin_menu', 'wpsus_menu_item');
+
